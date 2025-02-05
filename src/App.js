b491852559260d76d989709790af51818340f411
joanamorais13 <joanamorais13@gmail.com>
2025-02-05 14:36:38 +0000

    added weather component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
+import React from 'react';
 import './App.css';
+import Weather from './Weather';
 
-function App() {
+function App(){
   return (
     <div className="App">
-        <h1>Weather app</h1>
+    <div className="container">
+         <Weather />
+    </div>
+      <footer>
+        This project is open sourced on {" "}
+      <strong> 
+        <a href="https://github.com/joanamorais13/react-weather-project"  target="_blank"
+  rel="noopener noreferrer"
+          >GitHub</a></strong>
+        </footer>
+    
     </div>
   );
 }
---
+++ src/Weather.js
@@ -0,0 +1,48 @@
+import React from "react";
+import axios from "axios";
+import "./Weather.css"
+
+
+export default function Weather (){
+    return (
+        <div className="weather">
+            <form>
+                <div className="row">
+                    <div className="col-9">
+                <input type="search" placeholder="Enter a city..."  className="form-control" autoFocus="on"/>
+                </div>
+                 <div className="col-3">
+                <input type="submit" value="search" className="btn btn-primary w-100" /></div>
+                </div>
+            </form>
+            <div className="main-city">
+                <h1>New York</h1>
+            <ul>
+                <li>Wednesday 07:00</li>
+                <li>Mostly cloudy</li>
+            </ul>
+            </div>
+            
+            <div className="row">
+            <div className="col-6">
+                <div className="clearfix">
+                <img src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png" alt="mostly cloudy" className="float-start"  />
+                <div className="float-start">
+                <span className="temperature">6</span> 
+                <span className="unit">C°</span> 
+           </div>
+           </div>
+            </div>
+            <div className="col-6">
+                <ul>
+                    <li>
+                        Precipitation: 15%
+                    </li>
+                    <li>Humidity 72%</li>
+                    <li>Wind: 13 km/h</li>
+                </ul>
+            </div>
+            </div>
+        </div>
+    )
+}
