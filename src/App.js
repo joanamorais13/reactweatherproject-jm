b3093010e374a54816e5a77fd8e63ea3bd43a805
joanamorais13 <joanamorais13@gmail.com>
2025-02-28 22:10:14 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App(){
   return (
     <div className="App">
     <div className="container">
-         <Weather defaultiCity= "New York" />
+         <Weather defaultCity= "New York" />
     </div>
       <footer>
         This project is open sourced on {" "}
